bdda8caff09ff5572809e2b626df5d1b3adcba14
Duy Nguyen Duc Thanh <21520774@gm.uit.edu.vn>
2023-03-28 01:21:01 +0700

    Test

--- app/src/main/java/com/example/workmanagement/MainActivity.java
+++ app/src/main/java/com/example/workmanagement/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.workmanagement;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn;
+    TextView txt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.button);
+        txt = findViewById(R.id.text);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txt.setText("Á á á...");
+            }
+        });
+
     }
 }
