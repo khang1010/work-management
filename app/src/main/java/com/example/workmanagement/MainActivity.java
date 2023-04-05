a13934e97679f0c9f745f23bb19bf7ed84ae959d
NTKhang <21520963@gm.uit.edu.vn>
2023-04-06 00:56:29 +0700

    update splash screen

--- app/src/main/java/com/example/workmanagement/MainActivity.java
+++ app/src/main/java/com/example/workmanagement/MainActivity.java
@@ -12,18 +12,10 @@ import android.widget.TextView;
 import com.example.workmanagement.activities.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button btn;
-    TextView txt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        btn = findViewById(R.id.button);
-        txt = findViewById(R.id.text);
-
-        btn.setOnClickListener(view -> txt.setText("Duy ngáo"));
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -31,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, LoginActivity.class));
                 finish();
             }
-        }, 2000);
+        }, 3000);
 
     }
 }
