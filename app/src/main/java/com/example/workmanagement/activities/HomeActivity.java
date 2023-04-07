178c99b086df84a415c2819eeb3c3262b065cbf0
Quang Duong <quangduongptsc@gmail.com>
2023-04-07 22:37:08 +0700

    call login api in local

--- app/src/main/java/com/example/workmanagement/activities/HomeActivity.java
+++ app/src/main/java/com/example/workmanagement/activities/HomeActivity.java
@@ -2,10 +2,13 @@ package com.example.workmanagement.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
@@ -14,6 +17,9 @@ import com.example.workmanagement.databinding.ActivityHomeBinding;
 import com.example.workmanagement.fragments.ChatFragment;
 import com.example.workmanagement.fragments.HomeFragment;
 import com.example.workmanagement.fragments.SettingFragment;
+import com.example.workmanagement.utils.dto.UserDTO;
+import com.example.workmanagement.utils.services.impl.AuthServiceImpl;
+import com.example.workmanagement.viewmodels.User;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -22,10 +28,15 @@ import com.google.android.gms.tasks.OnSuccessListener;
 
 import kotlin.Unit;
 import kotlin.jvm.functions.Function1;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class HomeActivity extends AppCompatActivity {
 
     private ActivityHomeBinding binding;
+
+    private User user;
 
     GoogleSignInOptions gso;
     GoogleSignInClient gsc;
@@ -45,6 +56,33 @@ public class HomeActivity extends AppCompatActivity {
 
         if (account == null) {
             goSignOut();
+        } else {
+            UserDTO userDTO = new UserDTO();
+            userDTO.setEmail(account.getEmail());
+            userDTO.setDisplayName(account.getDisplayName());
+            userDTO.setFamilyName(account.getFamilyName());
+            userDTO.setGivenName(account.getGivenName());
+            userDTO.setPhotoUrl(String.valueOf(account.getPhotoUrl()));
+
+            AuthServiceImpl.getInstance().getService().loginUser(userDTO).enqueue(new Callback<UserDTO>() {
+                @Override
+                public void onResponse(Call<UserDTO> call, Response<UserDTO> response) {
+                    if(response.isSuccessful() && response.code() == 200) {
+                        Toast.makeText(HomeActivity.this, "Success", Toast.LENGTH_SHORT).show();
+                        user = new ViewModelProvider(HomeActivity.this).get(User.class);
+                        user.setId(response.body().getId());
+                        user.setEmail(response.body().getEmail());
+                        user.setDisplayName(response.body().getDisplayName());
+                        user.setPhotoUrl(response.body().getPhotoUrl());
+                        user.setToken(response.body().getToken());
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<UserDTO> call, Throwable t) {
+                    Toast.makeText(HomeActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
         }
 
         binding.logoutBtn.setOnClickListener(view -> {
--- app/src/main/java/com/example/workmanagement/activities/LoginActivity.java
+++ app/src/main/java/com/example/workmanagement/activities/LoginActivity.java
@@ -2,13 +2,18 @@ package com.example.workmanagement.activities;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.example.workmanagement.R;
 import com.example.workmanagement.databinding.ActivityLoginBinding;
+import com.example.workmanagement.utils.dto.UserDTO;
+import com.example.workmanagement.utils.services.impl.AuthServiceImpl;
+import com.example.workmanagement.viewmodels.User;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -16,16 +21,23 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class LoginActivity extends AppCompatActivity {
 
     private ActivityLoginBinding binding;
 
+    private User user;
+
     GoogleSignInOptions gso;
     GoogleSignInClient gsc;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding =ActivityLoginBinding.inflate(getLayoutInflater());
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
@@ -34,9 +46,9 @@ public class LoginActivity extends AppCompatActivity {
 
         gsc = GoogleSignIn.getClient(this, gso);
 
-        GoogleSignInAccount account =GoogleSignIn.getLastSignedInAccount(this);
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
 
-        if (account != null){
+        if (account != null) {
             goTohome();
         }
 
@@ -62,12 +74,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == 1234){
+        if (requestCode == 1234) {
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
 
             try {
                 task.getResult(ApiException.class);
+                GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+
                 goTohome();
+
             } catch (ApiException e) {
                 Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
             }
