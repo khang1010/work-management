2a8565b526ca81280aa1af5c26dab1a7fccb93c1
Quang Duong <quangduongptsc@gmail.com>
2023-04-27 09:10:56 +0700

    fix chat fragment

--- app/src/main/java/com/example/workmanagement/adapter/BoardMessageRecViewAdapter.java
+++ app/src/main/java/com/example/workmanagement/adapter/BoardMessageRecViewAdapter.java
@@ -18,6 +18,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.example.workmanagement.R;
 import com.example.workmanagement.activities.MessageActivity;
+import com.example.workmanagement.utils.dto.BoardInfo;
 import com.example.workmanagement.utils.dto.MessageDTO;
 
 import java.util.ArrayList;
@@ -25,7 +26,7 @@ import java.util.List;
 
 public class BoardMessageRecViewAdapter extends RecyclerView.Adapter<BoardMessageRecViewAdapter.ViewHolder> {
 
-    private ArrayList<MessageDTO> messageDTOS = new ArrayList<>();
+    private List<BoardInfo> boards;
 
     private Context context;
 
@@ -34,12 +35,10 @@ public class BoardMessageRecViewAdapter extends RecyclerView.Adapter<BoardMessag
     }
 
 
-    public BoardMessageRecViewAdapter() {
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position, @NonNull List<Object> payloads) {
-        super.onBindViewHolder(holder, position, payloads);
+    public BoardMessageRecViewAdapter(Context context) {
+        if (boards == null)
+            boards = new ArrayList<>();
+        this.context = context;
     }
 
     @NonNull
@@ -52,44 +51,21 @@ public class BoardMessageRecViewAdapter extends RecyclerView.Adapter<BoardMessag
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        if (messageDTOS != null && messageDTOS.size() > position) {
-            holder.displayName.setText(messageDTOS.get(position).getEmail());
-            holder.displayMessage.setText(messageDTOS.get(position).getMessage());
-        }
-        if (context != null) {
-            Glide.with(context)
-                    .load(messageDTOS.get(position).getPhotoUrl())
-                    .into(holder.imageAvatar);
-        }
-
-        holder.boardBox.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (context != null) {
-                    Intent intent = new Intent(view.getContext(), MessageActivity.class);
-                    //intent.putExtra("displayName", messageDTOS.get(position).getEmail());
-                    //intent.putExtra("displayMessage", messageDTOS.get(position).getMessage());
-                    view.getContext().startActivity(intent);
-                } else
-                    Toast.makeText(view.getContext(), "bi gi a ma", Toast.LENGTH_SHORT).show();
-
-            }
-        });
-
+        holder.displayName.setText(boards.get(position).getName());
+        Glide.with(context).asBitmap().load("https://scontent.fsgn5-2.fna.fbcdn.net/v/t39.30808-6/341694474_496163519267495_2965195482123986755_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=CVH65JLugRkAX_yXeAC&_nc_ht=scontent.fsgn5-2.fna&oh=00_AfA-t7f-Fu-h-KQQVxQFB8CCkMbsm4bIbXlh_HacakFiTA&oe=644EDA5E")
+                        .into(holder.imageAvatar);
+        holder.boardBox.setOnClickListener(view ->
+                view.getContext().startActivity(new Intent(view.getContext(), MessageActivity.class)));
     }
 
     @Override
     public int getItemCount() {
-        return messageDTOS.size();
+        return boards.size();
     }
 
-    public void setMessageDTOS(ArrayList<MessageDTO> messageDTOS) {
-        this.messageDTOS = messageDTOS;
-        this.context = context;
+    public void setBoards(List<BoardInfo> boards) {
+        this.boards = boards;
         notifyDataSetChanged();
-    }
-
-    public void setMessageDTOS(ArrayList<MessageDTO> messageDTOS, Context context) {
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
--- app/src/main/java/com/example/workmanagement/fragments/ChatFragment.java
+++ app/src/main/java/com/example/workmanagement/fragments/ChatFragment.java
@@ -3,7 +3,10 @@ package com.example.workmanagement.fragments;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -14,74 +17,43 @@ import android.view.ViewGroup;
 import com.example.workmanagement.R;
 import com.example.workmanagement.activities.MessageActivity;
 import com.example.workmanagement.adapter.BoardMessageRecViewAdapter;
+import com.example.workmanagement.databinding.FragmentChartBinding;
+import com.example.workmanagement.databinding.FragmentChatBinding;
+import com.example.workmanagement.databinding.FragmentTableBinding;
 import com.example.workmanagement.utils.dto.MessageDTO;
+import com.example.workmanagement.viewmodels.UserViewModel;
 
 import java.util.ArrayList;
 
 public class ChatFragment extends Fragment {
 
-    public ChatFragment() {
-        // Required empty public constructor
-    }
+    private FragmentChatBinding binding;
 
-    private RecyclerView boardMessBoxRecView;
-    private ArrayList<String> messBox = new ArrayList<>();
+    private BoardMessageRecViewAdapter adapter;
+
+    private UserViewModel userViewModel;
+
+    public ChatFragment() {}
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-
-        //boardMessBoxRecView = boardMessBoxRecView.findViewById(R.id.boardMessBoxRecView);
-
-
-
     }
-    BoardMessageRecViewAdapter adapter = new BoardMessageRecViewAdapter();
-    ArrayList<MessageDTO> messageDTOS = new ArrayList<>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        View rootView = inflater.inflate(R.layout.fragment_chat, container, false);
-        boardMessBoxRecView = rootView.findViewById(R.id.boardMessBoxRecView);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
-
-        boardMessBoxRecView.setLayoutManager(layoutManager);
-
-        //ArrayList<MessageDTO> messageDTOS = new ArrayList<>();
-        messageDTOS.add(new MessageDTO("ZangChuChe@gmail.com", "https://scontent.fsgn5-2.fna.fbcdn.net/v/t39.30808-6/341694474_496163519267495_2965195482123986755_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=CVH65JLugRkAX_yXeAC&_nc_ht=scontent.fsgn5-2.fna&oh=00_AfA-t7f-Fu-h-KQQVxQFB8CCkMbsm4bIbXlh_HacakFiTA&oe=644EDA5E", "Chao mung tro lai voi chanel cua minh"));
-        messageDTOS.add(new MessageDTO("DuyDan@gmail.com", "https://scontent.fsgn5-2.fna.fbcdn.net/v/t39.30808-6/341694474_496163519267495_2965195482123986755_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=CVH65JLugRkAX_yXeAC&_nc_ht=scontent.fsgn5-2.fna&oh=00_AfA-t7f-Fu-h-KQQVxQFB8CCkMbsm4bIbXlh_HacakFiTA&oe=644EDA5E", "Toi la chu be dan"));
-        messageDTOS.add(new MessageDTO("KhangDan@gmail.com", "https://scontent.fsgn5-2.fna.fbcdn.net/v/t39.30808-6/341694474_496163519267495_2965195482123986755_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=CVH65JLugRkAX_yXeAC&_nc_ht=scontent.fsgn5-2.fna&oh=00_AfA-t7f-Fu-h-KQQVxQFB8CCkMbsm4bIbXlh_HacakFiTA&oe=644EDA5E", "Zang cute qua"));
-        messageDTOS.add(new MessageDTO("ZkKhai@gmail.com", "https://scontent.fsgn5-2.fna.fbcdn.net/v/t39.30808-6/341694474_496163519267495_2965195482123986755_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=CVH65JLugRkAX_yXeAC&_nc_ht=scontent.fsgn5-2.fna&oh=00_AfA-t7f-Fu-h-KQQVxQFB8CCkMbsm4bIbXlh_HacakFiTA&oe=644EDA5E", "iu ck"));
-        messageDTOS.add(new MessageDTO("DuongDan@gmail.com", "https://scontent.fsgn5-2.fna.fbcdn.net/v/t39.30808-6/341694474_496163519267495_2965195482123986755_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=CVH65JLugRkAX_yXeAC&_nc_ht=scontent.fsgn5-2.fna&oh=00_AfA-t7f-Fu-h-KQQVxQFB8CCkMbsm4bIbXlh_HacakFiTA&oe=644EDA5E", "Toi khong suy"));
-
-        //BoardMessageRecViewAdapter adapter = new BoardMessageRecViewAdapter();
-        adapter.setMessageDTOS(messageDTOS);
-
-        boardMessBoxRecView.setAdapter(adapter);
-        boardMessBoxRecView.setLayoutManager(new LinearLayoutManager(getActivity()));
-
-
-//        Intent intent = new Intent(getActivity(), MessageActivity.class);
-//
-//        // Add any necessary data to the Intent
-//        intent.putExtra("message", "Messsage Screen");
-//
-//        // Start the MessageActivity
-//        startActivity(intent);
-
-        return rootView;
+        binding = FragmentChatBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
-    protected void loadData() {
-        // Perform network request to get data
-        // ...
-
-        // Update adapter with data and context
-        if (getContext() != null) {
-            adapter.setMessageDTOS(messageDTOS, getContext());
-        }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        userViewModel = new ViewModelProvider(getActivity()).get(UserViewModel.class);
+        adapter = new BoardMessageRecViewAdapter(getActivity());
+        binding.boardMessBoxRecView.setAdapter(adapter);
+        binding.boardMessBoxRecView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        userViewModel.getBoards().observe(getViewLifecycleOwner(), boards -> adapter.setBoards(boards));
     }
 }
