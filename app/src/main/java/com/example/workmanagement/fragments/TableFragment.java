4fd0c0ef2f49483a63c4ad2066970419e4058e8e
NTKhang <21520963@gm.uit.edu.vn>
2023-04-27 15:44:13 +0700

    update bug

--- app/src/main/java/com/example/workmanagement/fragments/TableFragment.java
+++ app/src/main/java/com/example/workmanagement/fragments/TableFragment.java
@@ -1,18 +1,26 @@
 package com.example.workmanagement.fragments;
 
+import android.app.Dialog;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Handler;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
@@ -22,6 +30,9 @@ import com.example.workmanagement.R;
 import com.example.workmanagement.activities.HomeActivity;
 
 import com.example.workmanagement.adapter.TableRecViewAdapter;
+import com.example.workmanagement.adapter.UserInvitedRecViewAdapter;
+import com.example.workmanagement.adapter.UserSearchInTaskAdapter;
+import com.example.workmanagement.adapter.UserSearchRecViewAdapter;
 import com.example.workmanagement.databinding.FragmentTableBinding;
 import com.example.workmanagement.tableview.TableViewAdapter;
 import com.example.workmanagement.tableview.TableViewListener;
@@ -29,12 +40,20 @@ import com.example.workmanagement.tableview.TableViewModel;
 import com.example.workmanagement.tableview.model.Cell;
 import com.example.workmanagement.tableview.model.ColumnHeader;
 import com.example.workmanagement.tableview.model.RowHeader;
+import com.example.workmanagement.utils.dto.SearchUserResponse;
+import com.example.workmanagement.utils.services.impl.UserServiceImpl;
 import com.example.workmanagement.viewmodels.BoardViewModel;
 import com.example.workmanagement.viewmodels.UserViewModel;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class TableFragment extends Fragment {
 
@@ -44,7 +63,7 @@ public class TableFragment extends Fragment {
 
     private RecyclerView tableRecView;
     private RelativeLayout clockLayout;
-
+    private Button btnAddTable;
     private UserViewModel userViewModel;
     public TableFragment() {}
 
@@ -94,5 +113,63 @@ public class TableFragment extends Fragment {
                 clockLayout.setVisibility(View.GONE);
             }, 500);
         });
+//        btnAddTable = view.findViewById(R.id.addTableBtn);
+//        btnAddTable.setOnClickListener(view1 -> {
+//           showCreateTableDialog();
+//        });
+    }
+
+    private void showCreateTableDialog() {
+
+        Dialog dialog = new Dialog(getActivity());
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setCancelable(true);
+        dialog.setContentView(R.layout.create_table);
+        EditText txtSearchUser = dialog.findViewById(R.id.editTxtSearchUserTable);
+        EditText txtTableName = dialog.findViewById(R.id.editTxtCreateTableName);
+        EditText txtTableDescription = dialog.findViewById(R.id.editTxtTableDes);
+        ConstraintLayout btnCreateTable = dialog.findViewById(R.id.btnCreateTable);
+
+
+        UserInvitedRecViewAdapter invitedAdapter = new UserInvitedRecViewAdapter(getActivity());
+        RecyclerView userInvitedRecView = dialog.findViewById(R.id.invitedUserRecView);
+        userInvitedRecView.setLayoutManager(new GridLayoutManager(getActivity(), 5));
+        userInvitedRecView.setAdapter(invitedAdapter);
+
+        UserSearchRecViewAdapter adapter = new UserSearchRecViewAdapter(getActivity(), invitedAdapter);
+        RecyclerView userRecView = dialog.findViewById(R.id.searchUserRecView);
+        userRecView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        userRecView.setAdapter(adapter);
+
+        txtSearchUser.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (!charSequence.toString().isEmpty())
+                    UserServiceImpl.getInstance().getService(userViewModel.getToken().getValue()).searchUser(1, charSequence.toString()).enqueue(new Callback<SearchUserResponse>() {
+                        @Override
+                        public void onResponse(Call<SearchUserResponse> call, Response<SearchUserResponse> response) {
+                            if (response.isSuccessful() && response.code() == 200)
+                                adapter.setUsers(response.body().getUsers());
+                        }
+
+                        @Override
+                        public void onFailure(Call<SearchUserResponse> call, Throwable t) {
+                            Toast.makeText(getActivity(), t.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                else adapter.setUsers(new ArrayList<>());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        dialog.show();
     }
 }
